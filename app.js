20a7884ccf9b47ed77699d8897421b50cdf5b5dd
aubbiali <aubbiali@sorint.it>
2022-08-27 15:33:28 +0200

    change var with const

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Create an express application object
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.set('view engine', 'pug');
 
